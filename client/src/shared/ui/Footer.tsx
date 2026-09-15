133711a22f902f9b51f0c33cf6f84f045d39c8b6
Andrew-Hardianto <andrewhardianto@gmail.com>
2026-09-15 16:45:02 +0700

    change theme

--- client/src/shared/ui/Footer.tsx
+++ client/src/shared/ui/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col className="text-center py-3" >
-                        <span className="font-weight-bold">Copyright &copy; BNI</span>
+                        <span className="font-weight-bold">Copyright &copy; Andrew Hardianto</span>
                     </Col>
                 </Row>
             </Container>
